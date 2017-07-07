5ef30a4c09c170aebc37b399c0effdb51690cbe3
夜禹 <mingyu.ymy@alipay.com>
2017-07-07 12:44:21 +0800

    nerd launcher

---
+++ NerdLauncher/app/src/main/java/com/example/yemingyu/nerdlauncher/SingleFragmentActivity.java
@@ -0,0 +1,31 @@
+package com.example.yemingyu.nerdlauncher;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yemingyu on 04/07/2017.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (null == fragment) {
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+    }
+
+}
